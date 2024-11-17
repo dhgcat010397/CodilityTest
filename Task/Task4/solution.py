182bd9aa8c0fa6e32d6705d140877de40405e99d
Đỗ Huy Gia Cát <dohuygiacat010397@gmail.com>
2024-11-17 21:04:26 +0700

    Update task 4
    Change algorithm

--- Task/Task4/solution.py
+++ Task/Task4/solution.py
@@ -1,8 +1,12 @@
 # Problem: https://app.codility.com/programmers/lessons/4-counting_elements/frog_river_one/
 
 def solution(X, A):
-    for idx in range(len(A)):
-        if set(list(range(1, X+1))).issubset(set(A[:idx+1])):
-            return idx
+    list_pos = [-1]*X
+    for idx, pos in enumerate(A):
+        if list_pos[pos-1] == -1:
+            list_pos[pos-1] = idx
 
-    return -1
+    if -1 in list_pos:
+        return -1
+    else:
+        return max(list_pos)
