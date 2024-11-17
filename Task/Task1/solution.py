d10b9c135cb9694b7da057387d4c77b107155ea0
Đỗ Huy Gia Cát <dohuygiacat010397@gmail.com>
2024-11-17 13:53:47 +0700

    complete taks 1

---
+++ Task/Task1/solution.py
@@ -0,0 +1,34 @@
+from typing import Counter
+
+def check_valid(S):
+    import re
+    return re.match(r'^[a-z]+$', S) is not None
+
+def solution(S):
+    if check_valid(S) is False:
+        return -1 
+    
+    all_substrings = []
+
+    # Find all substrings of the given string S
+    for i in range(len(S)):
+        for j in range(i+1, len(S)):
+            if i != j:
+                all_substrings.append(S[i:j+1])
+
+    # Get frequency of substrings
+    freq = Counter(all_substrings)
+
+    # Store all substrings which has frequency equal to 1 into a list
+    min_freq = []
+    for i in freq:
+        if freq[i] == 1:
+            min_freq.append(i)
+
+    # Get a hashmap with key is substring and value is the length of the substring
+    hashmap = dict()
+    for i in range(len(min_freq)):
+        hashmap[min_freq[i]] = len(min_freq[i])
+
+    return min(hashmap.values())
+
--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import sys
+from Task.Task1 import solution as task1
 from Task.Task2 import solution as task2
 
 
 if __name__ == "__main__":
-    print("Task 2")
-    moves = input("Input your moves: ")
-    count = task2.solution(moves)
-    if count == -1 or len(moves) > 50:
-        print("You didn't input your moves or your moves are incorrect!")
+    print("Task 1")
+    S = input("Input your string: ")
+    result = task1.solution(S)
+    if result == -1 or len(S) not in range(1,200):
+        print("You didn't input your string or your string are incorrect!")
     else:
-        print(count)
+        print(result)
+
+    # print("Task 2")
+    # moves = input("Input your moves: ")
+    # count = task2.solution(moves)
+    # if count == -1 or len(S) not in range(1,50):
+    #     print("You didn't input your moves or your moves are incorrect!")
+    # else:
+    #     print(count)
     
+    # print("Task 3")
+    # S = input("Input your string: ")
+    # result = task1.solution(S)
+    # if result == -1 or len(S) not in range(1,200):
+    #     print("You didn't input your string or your string are incorrect!")
+    # else:
+    #     print(result)
