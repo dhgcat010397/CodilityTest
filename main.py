1db677dde4798781dd965787b8b40edd04cac9bb
Đỗ Huy Gia Cát <dohuygiacat010397@gmail.com>
2024-11-28 19:46:34 +0700

    Add Task 7

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import sys
-from Task.Task1 import solution as task1
-from Task.Task2 import solution as task2
-from Task.Task3 import solution as task3
-from Task.Task4 import solution as task4
-from Task.Task5 import solution as task5
-from Task.Task6 import solution as task6
-
 
 if __name__ == "__main__":
     # print("Task 1")
+    # from Task.Task1 import solution as task1
     # S = input("Input your string: ")
     # result = task1.solution(S)
     # if result == -1 or len(S) not in range(1,200):
@@ -17,6 +10,7 @@ if __name__ == "__main__":
     #     print(result)
 
     # print("Task 2")
+    # from Task.Task2 import solution as task2
     # moves = input("Input your moves: ")
     # count = task2.solution(moves)
     # if count == -1 or len(S) not in range(1,50):
@@ -25,6 +19,7 @@ if __name__ == "__main__":
     #     print(count)
     
     # print("Task 3")
+    # from Task.Task3 import solution as task3
     # S = input("Input your string: ")
     # count = task3.solution(S)
     # if len(S) not in range(1,20):
@@ -33,6 +28,7 @@ if __name__ == "__main__":
     #     print(count)
 
     # print("Task 4")
+    # from Task.Task4 import solution as task4
     # X = int(input("Input your X: "))
     # N = int(input("Input size of your array A: "))
     # print("Enter you array A:")
@@ -47,6 +43,7 @@ if __name__ == "__main__":
     #     print(f"The earliest time when the frog can jump to the other side of the river is: {count} seconds")
     
     # print("Task 5")
+    # from Task.Task5 import solution as task5
     # result_1 = task5.solution([3, 1, 2])
     # result_2 = task5.solution([1, 2, 3, 4])
     # result_3 = task5.solution([7, 7, 7])
@@ -58,6 +55,7 @@ if __name__ == "__main__":
     # print(f"Given T = [10000], return: {result_4}")
 
     print("Task 6")
+    from Task.Task6 import solution as task6
     result_1 = task6.solution([2, 4, 1, 3, 4, 6, 2, 4, 1, 6])
     result_2 = task6.solution([5, 1, 2, 6, 6, 1, 3, 1, 4, 3, 4, 3, 4, 6, 1, 2, 4, 1, 6, 2])
     result_3 = task6.solution([1, 5, 3, 3, 1, 3])
@@ -67,3 +65,15 @@ if __name__ == "__main__":
     print(f"Given A = [5, 1, 2, 6, 6, 1, 3, 1, 4, 3, 4, 3, 4, 6, 1, 2, 4, 1, 6, 2], return: {result_2}")
     print(f"Given A = [1, 5, 3, 3, 1, 3], return: {result_3}")
     print(f"Given A = [3, 4], return: {result_4}")
+
+    # print("Task 7")
+    # from Task.Task7 import solution as task7
+    # result_1 = task6.solution([2, 4, 1, 3, 4, 6, 2, 4, 1, 6])
+    # result_2 = task6.solution([5, 1, 2, 6, 6, 1, 3, 1, 4, 3, 4, 3, 4, 6, 1, 2, 4, 1, 6, 2])
+    # result_3 = task6.solution([1, 5, 3, 3, 1, 3])
+    # result_4 = task6.solution([3, 4])
+    # print(f"The minimum number of domino tiles that must be removed:")
+    # print(f"Given A = [2, 4, 1, 3, 4, 6, 2, 4, 1, 6], return: {result_1}")
+    # print(f"Given A = [5, 1, 2, 6, 6, 1, 3, 1, 4, 3, 4, 3, 4, 6, 1, 2, 4, 1, 6, 2], return: {result_2}")
+    # print(f"Given A = [1, 5, 3, 3, 1, 3], return: {result_3}")
+    # print(f"Given A = [3, 4], return: {result_4}")
